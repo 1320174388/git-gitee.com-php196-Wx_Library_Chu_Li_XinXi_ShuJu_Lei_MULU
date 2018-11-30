482e1f341651ec0b3bdf329d3fd28ae220120ae4
时广玉 <shiguangyu@dlaotianhuang.com>
2018-11-30 16:50:51 +0800

    Optimizing RSD Function Return Class

--- shell.sh
+++ shell.sh
@@ -2,22 +2,42 @@ git status
 
 git add .
 
+git status
+
 git commit -m 'Optimizing RSD Function Return Class'
+
+git status
 
 git push mayun ShiGuangYu
 
+git status
+
 git push gitlab ShiGuangYu
+
+git status
 
 git push github ShiGuangYu
 
+git status
+
 git checkout master
+
+git status
 
 git merge ShiGuangYu
 
+git status
+
 git push mayun master
+
+git status
 
 git push gitlab master
 
+git status
+
 git push github master
+
+git status
 
 git checkout ShiGuangYu
